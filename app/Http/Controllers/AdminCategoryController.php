68cd696a35e3e283fc78dff5315e676683fb5541
michalwrona.dev@gmail.com <michalwrona.dev@gmail.com>
2020-02-09 08:32:45 +0100

    admin category

---
+++ app/Http/Controllers/AdminCategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class AdminCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return view('admin.category.index', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $validatedData = $request->validate([
+            'name' => 'required|unique:categories',
+        ]);
+        $category  = Category::create($input);
+        return redirect()->route('admin.category.index')->with('alert-success', "Category".$category->name." has been created!");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+        $categories = Category::pluck('name', 'id')->all();
+        return view('admin.category.edit', compact('category', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+        $validatedData = $request->validate([
+            'name' => 'required',
+        ]);
+        $category  = Category::findOrFail($id)->update($input);
+
+        return redirect()->route('admin.category.index')->with('alert-success', "Category  has been updated!");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Category::findOrfail($id)->delete();
+        return redirect()->route('admin.category.index')->with('alert-success', "Category  has been deleted!");
+    }
+}
